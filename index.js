9627afc13c8c28ca6d32b57347c74849786805b1
Zakaria Belassal <zakariabelassal@gmail.com>
2022-06-16 22:40:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,30 +10,95 @@ let square3 = document.getElementById('square3')
 let btnYellow = document.getElementById('positionColorYel')
 let square4 = document.getElementById('square4')
 
+let statusSqr1 = false;
+let statusSqr2 = false;
+let statusSqr3 = false;
+let statusSqr4 = false;
+
+let positionColorBlu = false;
+let positionColorRed = false;
+let positionColorGre = false;
+let positionColorYel = false;
+
+
 square1.addEventListener('click',()=>{
+    statusSqr1 = true;
+    statusSqr2 = false;
+    statusSqr3 = false;
+    statusSqr4 = false;
+   square1.classList.add('border','border-2','border-danger')
     btnBlue.addEventListener('click',()=>{
-        // square1.style.position="absolute";
-        square1.style.transform="translate3d(275px,220px,0px)"
+             
+             positionColorBlu = true;
+             positionColorRed = false;
+             positionColorGre = false;
+             positionColorYel = false;
+
+        if(statusSqr1 && positionColorBlu){
+        square1.style.transform="translate3d(463px,220px,0px)"
+        }
     })
 })
 
 square2.addEventListener('click',()=>{
     btnRed.addEventListener('click',()=>{
-        // square1.style.position="absolute";
-        square2.style.transform="translate3d(276px,-110px,0px)"
+        statusSqr1 = false;
+        statusSqr2 = true;
+        statusSqr3 = false;
+        statusSqr4 = false;
+        
+        positionColorBlu = false;
+        positionColorRed = true;
+        positionColorGre = false;
+        positionColorYel = false;
+        if(statusSqr2 && positionColorRed){
+        square2.style.transform="translate3d(463px,-110px,0px)"
+        }
     })
 })
 
 square3.addEventListener('click',()=>{
+    statusSqr1 = false;
+    statusSqr2 = false;
+    statusSqr3 = true;
+    statusSqr4 = false;
     btnGreen.addEventListener('click',()=>{
-        // square1.style.position="absolute";
-        square3.style.transform="translate3d(275px,110px,0px)"
+
+        positionColorBlu = false;
+        positionColorRed = false;
+        positionColorGre = true;
+        positionColorYel = false;
+
+        if(statusSqr3 && positionColorGre){
+        square3.style.transform="translate3d(463px,110px,0px)"
+        }
     })
 })
 
 square4.addEventListener('click',()=>{
+    statusSqr1 = false;
+    statusSqr2 = false;
+    statusSqr3 = false;
+    statusSqr4 = true;
     btnYellow.addEventListener('click',()=>{
-        // square1.style.position="absolute";
-        square4.style.transform="translate3d(276px,-220px,0px)"
+
+        positionColorBlu = false;
+        positionColorRed = false;
+        positionColorGre = false;
+        positionColorYel = true;
+
+        if(statusSqr4 && positionColorYel){
+        square4.style.transform="translate3d(463px,-220px,0px)"
+        }
     })
 })
+
+
+
+
+
+
+
+
+
+  
